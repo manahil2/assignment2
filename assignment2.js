5a058180520c76af52d0596beb2fbf8a961952b1
Manahil Aleem <manahilaleem1@gmail.com>
2024-12-15 17:30:12 +0500

    deploy

---
+++ assignment2.js
@@ -0,0 +1,38 @@
+//1
+let username;
+
+//2
+let myName = "Manahil Aleem";
+
+//3
+let message;
+message = "Hello World!";
+alert(message);
+
+//4
+let studentname = "Manahil";
+let studentage = 20;
+let certification = "Certified Web and App Developer";
+alert(studentname);
+alert(studentage);
+alert(certification);
+
+//5
+let pizzapattern = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizzapattern);
+
+//6
+let email = "manahilaleem1@gmail.com";
+alert("My email address is "+ email);
+
+//7
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to leaen from the book "+book);
+
+//8
+document.write("Yah!I can write HTML content through JavaScript");
+
+//9
+let specialstring = ("▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬");
+alert(specialstring);
+
